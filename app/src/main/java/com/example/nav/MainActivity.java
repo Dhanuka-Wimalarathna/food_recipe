21f5d09bad49555f45003f9b7d5132962b7393e9
Dhanuka-Wimalarathna <143159213+Dhanuka-Wimalarathna@users.noreply.github.com>
2024-10-04 01:05:18 +0530

    Initial commit

---
+++ app/src/main/java/com/example/nav/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.nav;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import com.example.nav.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        replaceFragment(new HomeFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            if (item.getItemId() == R.id.home) {
+                replaceFragment(new HomeFragment());
+            } else {
+                replaceFragment(new ProfileFragment());
+            }
+
+            return true;
+        });
+    }
+
+    public void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.commit();
+    }
+}
